976d7af6c0be2d29ec03381a2ef8cd8183b1d780
dewiashariii <dewiashari84@gmail.com>
2022-10-20 15:36:35 +0700

    first commit

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Dewi</li>
+        <li>Nama Belakang : Ashari</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Berua Raya No.12</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Makassar</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulu Tangkis</li>
+        <li>Lompat Tali</li>
+    </ul>
+</section>
